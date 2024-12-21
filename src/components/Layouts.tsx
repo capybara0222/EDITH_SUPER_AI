318c93efb6d32ab078bc6f664fa409bbe91e5fcf
Dane Manji <danemanji0089@gmail.com>
2024-12-20 22:22:24 -0600

    fix: correct some styles such as shadow

--- src/components/Layouts.tsx
+++ src/components/Layouts.tsx
@@ -5,32 +5,32 @@ import { Outlet } from "react-router-dom";
 import Tabs from "./Tabs";
 
 const Layouts = () => {
-  // const { initData } = retrieveLaunchParams();
+  const { initData } = retrieveLaunchParams();
 
-  // const fetchUser = async () => {
-  //   if (initData && initData.user) {
-  //     console.log(initData.user);
-  //     await axios
-  //       .post("https://b56c-162-251-62-70.ngrok-free.app/api/user", {
-  //         user_id: initData.user.id,
-  //         refer_code: "string",
-  //       })
-  //       .then((response) => {
-  //         if (response.status === 200) {
-  //           console.log("Success", response.data)
-  //         }
-  //         else {
-  //           console.log("Error", response.data)
-  //         }
-  //       })
-  //       .catch((error) => console.log("Error", error));
-  //   } else {
-  //     console.log("User data is not available");
-  //   }
-  // };
+  const fetchUser = async () => {
+    if (initData && initData.user) {
+      console.log(initData.user);
+      await axios
+        .post("https://b56c-162-251-62-70.ngrok-free.app/api/user", {
+          user_id: initData.user.id,
+          refer_code: "string",
+        })
+        .then((response) => {
+          if (response.status === 200) {
+            console.log("Success", response.data)
+          }
+          else {
+            console.log("Error", response.data)
+          }
+        })
+        .catch((error) => console.log("Error", error));
+    } else {
+      console.log("User data is not available");
+    }
+  };
 
   useEffect(() => {
-    // fetchUser();
+    fetchUser();
   }, []);
 
   return (
